98aa29c7ea4833ff3726e33eef4fdd7c2b0dd37d
Dong Nguyen <d.nguyen102599@gmail.com>
2024-05-01 16:02:37 -0500

    test 6

--- src/test/java/uta/cse3310/PlayerTest.java
+++ src/test/java/uta/cse3310/PlayerTest.java
@@ -9,11 +9,12 @@ public class PlayerTest extends TestCase {
     public void testPlayerInitialization() {
         WebSocket socket = Mockito.mock(WebSocket.class);
         Player player = new Player("Alice", socket);
-        
+        Player player2 = new Player("abcdefghijklmnopqrstuvwxyz", socket);
         assertEquals("Alice", player.name);
         assertEquals(0, player.score);
         assertEquals(false, player.isReady);
         assertEquals(socket, player.playerConn);
+        assertFalse(player2.name.length() <= 25);
     }
 
 }
